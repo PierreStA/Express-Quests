f0e165acebc906ee354ea358ded9f179a1fa3fbd
Pierre Saint Aubin <p.saintaubin@hotmail.fr>
2023-03-20 11:09:48 +0100

    express6 get en detail

--- movieHandlers.js
+++ movieHandlers.js
@@ -3,55 +3,57 @@ const database = require("./database");
 
 const getMovies = (req, res) => {
 
-  database
+  const initialSql = "select * from movies";
+  const where = [];
 
-    .query("select * from movies")
-
-    .then(([movies]) => {
-
-      res.json(movies);
-
-    })
-
-    .catch((err) => {
-
-      console.error(err);
-
-      res.status(500).send("Error retrieving data from database");
-
+  if (req.query.color != null) {
+    where.push({
+      column: "color",
+      value: req.query.color,
+      operator: "=",
     });
+  }
 
+  if (req.query.max_duration != null) {
+    where.push({
+      column: "duration",
+      value: req.query.max_duration,
+      operator: "<=",
+    });
+  }
+
+  database
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
-
   const id = parseInt(req.params.id);
-
-
   database
-
     .query("select * from movies where id = ?", [id])
-
     .then(([movies]) => {
-
       if (movies[0] != null) {
-
         res.json(movies[0]);
-
       } else {
-
         res.status(404).send("Not Found");
-
       }
-
     })
-
     .catch((err) => {
-
       console.error(err);
-
       res.status(500).send("Error retrieving data from database");
-
     });
 
 };
